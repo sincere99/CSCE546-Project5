270cee9d5c5fa803c68634ee4e3d7afcef7c7029
Sincere Dixon <sincere@email.sc.edu>
2022-03-21 00:41:57 -0400

    Deleting images

--- app/src/main/java/com/example/csce546_project5/MainActivity.kt
+++ app/src/main/java/com/example/csce546_project5/MainActivity.kt
@@ -11,61 +11,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rvContacts = findViewById<RecyclerView>(R.id.characterBios)
-        val contacts: List<Character> = createBios()
+        val contacts: List<Character> = createContacts()
 
 
         rvContacts.adapter = ContactsAdapter(this, contacts)
         rvContacts.layoutManager = LinearLayoutManager(this)
     }
 
-    private fun createBios(): List<Character> {
+    private fun createContacts(): List<Character> {
         val bios: MutableList<Character> = mutableListOf()
 
         //Pull information from resources
         //val comicNames = resources.getStringArray(android.R.array.comic_names)
         //val comicDesc = resources.getStringArray(android.R.array.comic_descriptions)
-        val charName = listOf("Bruce Wayne", "Alfred Pennyworth", "Dick Grayson", "Barbara Gordon",
-            "Jason Todd", "Tim Drake", "Stephanie Brown", "Cassandra Cain", "Damian Wayne", "Duke Thomas")
-        val charDescr = listOf("Batman", "Trusted Butler", "First Robin, later Nightwing", "First Batgirl, later Oracle",
-        "Second Robin, later Red Hood", "Third Robin, later Red Robin", "Third Batgirl, forth Robin, later Spoiler", "Second Batgirl, Later Black Bat", 
-            "Fifth Robin", "The Signal")
+        val charName = listOf(
+            "Bruce Wayne",
+            "Alfred Pennyworth",
+            "Dick Grayson",
+            "Barbara Gordon",
+            "Jason Todd",
+            "Tim Drake",
+            "Stephanie Brown",
+            "Cassandra Cain",
+            "Damian Wayne",
+            "Duke Thomas"
+        )
+        val charDescr = listOf(
+            "Batman",
+            "Trusted Butler",
+            "First Robin, later Nightwing",
+            "First Batgirl, later Oracle",
+            "Second Robin, later Red Hood",
+            "Third Robin, later Red Robin",
+            "Third Batgirl, forth Robin, later Spoiler",
+            "Second Batgirl, Later Black Bat",
+            "Fifth Robin",
+            "The Signal"
+        )
 
 
         for (i in charName.indices) {
-            var image: Any
-            when (i) {
-                0 -> {
-                    image = R.drawable.brucewayne
-                }
-                1 -> {
-                    image = R.drawable.alfredpennyworth
-                }
-                2 -> {
-                    image = R.drawable.dickgrayson
-                }
-                3 -> {
-                    image = R.drawable.barbaragordon
-                }
-                4 -> {
-                    image = R.drawable.jasontodd
-                }
-                5 -> {
-                    image = R.drawable.timdrake
-                }
-                6 -> {
-                    image = R.drawable.stephaniebrown
-                }
-                7 -> {
-                    image = R.drawable.cassandracain
-                }
-                8 -> {
-                    image = R.drawable.damianwayne
-                }
-                else -> {
-                    image = R.drawable.dukethomas
-                }
-            }
-            bios.add(Character(charName[i], charDescr[i] , image))
+            bios.add(Character(charName[i], charDescr[i], R.drawable.brucewayne))
         }
         return bios
     }
